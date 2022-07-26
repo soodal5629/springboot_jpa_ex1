6c5310c7aa9d66d189052ed1beeec9780001b7bd
chaelynjang <chaelynjang@lotte.net>
2022-07-26 20:45:48 +0900

    상품 엔티티 재고 수량 관련 비즈니스 로직 추가, 상품 repository 개발

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,5 +25,23 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    /* 데이터를 가지고있는 쪽에 비즈니스 로직을 두는 것이 더 객체지향적이고 응집력이 높다. */
+    /*
+    * 재고수량 증가
+    * */
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+    /*
+    * 재고수량 감소
+    * */
+    public void removeStock(int quantity){
+        int resStock = this.stockQuantity - quantity;
+        if(resStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = resStock;
+    }
 
 }
