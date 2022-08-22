58cfed64b57a7cb092f89124fc803d11d120ee63
chaelynjang <chaelynjang@lotte.net>
2022-08-22 21:30:46 +0900

    회원 조회 api 개발

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,6 +19,31 @@ import javax.validation.constraints.NotEmpty;
 public class MemberApiController {
     private final MemberService memberService;
 
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1(){
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result memberV2(){
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getUsername()))
+                .collect(Collectors.toList()); // List<Member> -> List<MemberDto> 로 변환
+        return new Result(collect.size(), collect);
+    }
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto{
+        private String name;
+    }
     /* 회원 등록 api */
     @PostMapping("/api/v1/members") // 엔티티 자체를 파라미터로 절대 받지 마세요!! --> api 스펙이 바뀔 수 있음
     // 또한 엔티티는 외부에 노출하면 안됨
